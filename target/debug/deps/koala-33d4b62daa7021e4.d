5a287cec3017ca9fb5ee9bd1bb3f7ad392ae6256
Ojima Abraham <abrahamojima2018@gmail.com>
2023-07-26 01:30:47 -0400

    Migrate implementation to Rust (#11)
    * Removed pre-commit hook
    
    * Removed pre-commit hook
    
    * Modified CI
    
    * Modified CI

--- target/debug/deps/koala-33d4b62daa7021e4.d
+++
@@ -1,5 +0,0 @@
-/Users/ojima/Desktop/koala/target/debug/deps/koala-33d4b62daa7021e4.rmeta: src/main.rs
-
-/Users/ojima/Desktop/koala/target/debug/deps/koala-33d4b62daa7021e4.d: src/main.rs
-
-src/main.rs:
