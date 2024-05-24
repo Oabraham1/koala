6988129b2ae18a25e2b0d8b57856414712bff820
Ojima <ojima.abraham@viam.com>
2024-05-24 09:19:33 -0400

    Processes merged

--- internal/process/process.go
+++ internal/process/process.go
@@ -4,8 +4,16 @@
 package process
 
 import (
+	"bytes"
+	"encoding/binary"
 	"fmt"
+	"io"
+	"os"
 	"runtime"
+	"strconv"
+	"strings"
+	"syscall"
+	"unsafe"
 )
 
 type Process struct {
@@ -37,6 +45,25 @@ type Process struct {
 	// This is the process ID of the parent process.
 	Ppid int32
 }
+
+type kernelInfoProcess struct {
+	_      [40]byte
+	Pid    int32
+	_      [199]byte
+	Comm   [16]byte
+	_      [301]byte
+	PPid   int32
+	_      [84]byte
+	Status byte
+	_      [1]byte
+}
+
+const (
+	_CTRL_KERN               = 1
+	_KERN_PROC               = 14
+	_KERN_PROC_ALL           = 0
+	_KERNEL_INFO_STRUCT_SIZE = 648
+)
 
 // GetAllProcesses returns a list of all processes.
 //
@@ -116,3 +143,168 @@ func getOpenBSDProcesses() ([]Process, error) {
 // func getOpenBSDProcessParent(pid int32) (Process, error) {
 // 	return Process{}, nil
 // }
+
+func getLinuxProcesses() ([]Process, error) {
+	data, err := os.Open("/proc")
+	if err != nil {
+		return nil, err
+	}
+	defer data.Close()
+
+	processes := make([]Process, 0, 50)
+	for {
+		files, err := data.Readdirnames(10)
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return nil, err
+		}
+
+		for _, file := range files {
+			pid, err := strconv.Atoi(file)
+			if err != nil {
+				continue
+			}
+
+			staticPath := fmt.Sprintf("/proc/%d/stat", pid)
+			staticFile, err := os.ReadFile(staticPath)
+			if err != nil {
+				return nil, err
+			}
+
+			data := string(staticFile)
+			dataBytesStart := strings.IndexRune(data, '(') + 1
+			dataBytesEnd := strings.IndexRune(data, ')')
+			dataBytesName := data[dataBytesStart : dataBytesStart+dataBytesEnd]
+			process := Process{
+				Pid:  int32(pid),
+				Name: dataBytesName,
+			}
+
+			_, err = fmt.Scanf(data, "%d %s", &process.Status, &process.Ppid)
+			if err != nil {
+				return nil, err
+			}
+
+			processes = append(processes, process)
+		}
+	}
+	return processes, nil
+}
+
+func getLinuxProcess(pid int32) (Process, error) {
+	processes, err := getLinuxProcesses()
+	if err != nil {
+		return Process{}, err
+	}
+
+	for _, process := range processes {
+		if process.Pid == pid {
+			return process, nil
+		}
+	}
+	return Process{}, nil
+}
+
+func getLinuxProcessParent(pid int32) (Process, error) {
+	processes, err := getLinuxProcesses()
+	if err != nil {
+		return Process{}, err
+	}
+
+	for _, process := range processes {
+		if process.Pid == pid {
+			return getLinuxProcess(process.Ppid)
+		}
+	}
+	return Process{}, nil
+}
+
+func getMacOSProcesses() ([]Process, error) {
+	memory_io_buffer := [4]int32{_CTRL_KERN, _KERN_PROC, _KERN_PROC_ALL, 0}
+	size := uintptr(0)
+
+	_, _, err := syscall.Syscall6(
+		syscall.SYS___SYSCTL,
+		uintptr(unsafe.Pointer(&memory_io_buffer[0])),
+		uintptr(4),
+		uintptr(0),
+		uintptr(unsafe.Pointer(&size)),
+		uintptr(0),
+		uintptr(0))
+
+	if err != 0 {
+		return nil, err
+	}
+
+	systemCallBuffer := make([]byte, size)
+
+	_, _, err = syscall.Syscall6(
+		syscall.SYS___SYSCTL,
+		uintptr(unsafe.Pointer(&memory_io_buffer[0])),
+		uintptr(4),
+		uintptr(unsafe.Pointer(&systemCallBuffer[0])),
+		uintptr(unsafe.Pointer(&size)),
+		uintptr(0),
+		uintptr(0))
+
+	if err != 0 {
+		return nil, err
+	}
+
+	bytesBuffer := bytes.NewBuffer(systemCallBuffer[0:size])
+
+	processes := make([]*kernelInfoProcess, 0, 50)
+	kernel := 0
+	for i := _KERNEL_INFO_STRUCT_SIZE; i < bytesBuffer.Len(); i += _KERNEL_INFO_STRUCT_SIZE {
+		process := &kernelInfoProcess{}
+		err := binary.Read(bytes.NewBuffer(bytesBuffer.Bytes()[kernel:i]), binary.LittleEndian, process)
+		if err != nil {
+			return nil, err
+		}
+
+		kernel = i
+		processes = append(processes, process)
+	}
+
+	result := make([]Process, 0, len(processes))
+	for _, process := range processes {
+		result = append(result, Process{
+			Pid:    process.Pid,
+			Name:   string(process.Comm[:bytes.IndexByte(process.Comm[:], 0)]),
+			Ppid:   process.PPid,
+			Status: string([]byte{process.Status}),
+		})
+	}
+
+	return result, nil
+}
+
+func getMacOSProcess(pid int32) (Process, error) {
+	processes, err := getMacOSProcesses()
+	if err != nil {
+		return Process{}, err
+	}
+
+	for _, process := range processes {
+		if process.Pid == pid {
+			return process, nil
+		}
+	}
+	return Process{}, nil
+}
+
+func getMacOSProcessParent(pid int32) (Process, error) {
+	processes, err := getMacOSProcesses()
+	if err != nil {
+		return Process{}, err
+	}
+
+	for _, process := range processes {
+		if process.Pid == pid {
+			return getMacOSProcess(process.Ppid)
+		}
+	}
+	return Process{}, nil
+}
